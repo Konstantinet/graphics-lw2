37b15391a59d1887ff67c4e663af6d5b4ff52d6e
Konstantinet <kostya222lb@mail.ru>
2022-04-15 18:34:50 +0500

    Transitions

--- test/test.cpp
+++ test/test.cpp
@@ -2,25 +2,60 @@
 #include <GL/glut.h>
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 GLuint VBO;
-GLuint VBO2;
+GLuint gWorldLocation;
+float Scale = 0.0;
+glm::vec3 Vertices[6];
+GLdouble* MultMatrix(GLdouble*, GLdouble*);
+
 
 void RenderSceneCB()
 {
-    glClear(GL_COLOR_BUFFER_BIT);//fill window with set colour
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);//fill window with set colour
     glEnableVertexAttribArray(0);//enabling reading from array buffer
+    glLoadIdentity();
+    //if (Scale < 0.5)
+        Scale += 0.0006;
+    //if (Scale > 0.6)
+      //  Scale -= 0, 00006;
+    
+    glTranslatef(sinf(Scale), 0, 0);
+    glRotatef(sinf(Scale)*500, 0, 0,1);
+    glScalef(abs(sinf(Scale)), abs(sinf(Scale)), abs(sinf(Scale)));
+    
+    //glLoadMatrixf(value_ptr(glm::mat4(1.0)*World));
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);//specify points rendering attributes stored in VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glDrawArrays(GL_POLYGON, 0, 3);//draw 
-    glDrawArrays(GL_POLYGON, 3, 3);//draw 
+    glDrawArrays(GL_TRIANGLES, 0, 3);//draw 
+    glDrawArrays(GL_TRIANGLES, 3, 3);//draw 
     glDisableVertexAttribArray(0);//disabling
+
+    
+   
+    
     glutSwapBuffers();//double bufferisation
+    glutPostRedisplay();
+   
+}
+GLdouble* MultMatrix(GLdouble* a, GLdouble* b) {
+    GLdouble* c = new double[16];
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            c[i * 4 + j] = 0;
+            for (int k = 0; k < 4; ++k)
+                c[i * 4 + j] += a[i * 4 + k] * b[k * 4 + j];
+        }
+    }
+    return c;
 }
 void CreateVertex() {
     
-    glm::vec3 Vertices[6];
-    Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+    Vertices[0] = glm::vec3(0.1f, 0.0f, 0.0f);
     Vertices[1] = glm::vec3(0.5f, 0.5f, 0.0f);
     Vertices[2] = glm::vec3(0.5f, -0.5f, 0.0f);
     Vertices[3] = glm::vec3(-0.1f, 0.0f, 0.0f);
@@ -28,7 +63,7 @@ void CreateVertex() {
     Vertices[5] = glm::vec3(-0.5f, -0.5f, 0.0f);
     glGenBuffers(6, &VBO);//creates VBO buffer of reqired size VBO stored in graphic memory
     glBindBuffer(GL_ARRAY_BUFFER, VBO);//bind buffer to default gl buffer
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);//fill gl array buffer;4th arg - pattern of data store
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STREAM_DRAW);//fill gl array buffer;4th arg - pattern of data store
 }
 int main(int argc, char** argv)
 {
@@ -39,9 +74,8 @@ int main(int argc, char** argv)
     glutCreateWindow("Tutorial 01");
    
     glutDisplayFunc(RenderSceneCB);//set function to be used inside the main
+    glutIdleFunc(RenderSceneCB);
     glClearColor(1.0f, 0.0f, 1.0f, 0.0f);//set colour to gl colour buffer
-    
-
     
     GLenum res = glewInit();
     if (res !=GLEW_OK)
@@ -49,6 +83,8 @@ int main(int argc, char** argv)
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
     }
+
+
     CreateVertex();
 
     glutMainLoop();
